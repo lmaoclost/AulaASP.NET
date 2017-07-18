9caeeaf9630e91f663bcd7079bcc8fb026972347
Renan <renansmoliveira@gmail.com>
2017-07-18 18:01:46 -0300

    Sys

---
+++ ProjetoModeloDDD/ProjetoModeloDDD.Domain/Services/ClienteService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.Domain.Interfaces;
+using ProjetoModeloDDD.Domain.Interfaces.Services;
+using System.Linq;
+
+namespace ProjetoModeloDDD.Domain.Services
+{
+    public class ClienteService : ServiceBase<Cliente>, IClienteService
+    {
+        private readonly IClienteRepository _clienteRepository;
+
+        public ClienteService(IClienteRepository clienteRepository) :base(clienteRepository)
+        {
+
+        }
+
+        public ClienteService(IRepositoryBase<Cliente> repository) : base(repository)
+        {
+        }
+
+        public IEnumerable<Cliente> ObterClientesEspeciais(IEnumerable<Cliente> clientes)
+        {
+            return clientes.Where(c => c.ClienteEspecial(c));
+        }
+    }
+}
